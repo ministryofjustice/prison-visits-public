3e9cf874d53d3940c1b5bc5b75d301c0f72700b6
Martyn Loughran <me@mloughran.com>
2016-01-22 18:16:07 +0000

    Fetch prison details from API (fixes visitor step)

--- app/models/prison.rb
+++ app/models/prison.rb
@@ -1,8 +1,23 @@
 class Prison
   include NonPersistedModel
 
+  MAX_VISITORS = 6
+
   attribute :id, String
   attribute :name, String
+  attribute :address, String
+  attribute :postcode, String
+  attribute :email_address, String
+  attribute :phone_no, String
+
+  def finder_slug
+    'TODO' # Not yet reported by API
+  end
+
+  def self.find_by_id(id)
+    result = Rails.configuration.api.get("/prisons/#{id}")
+    Prison.new(result['prison'])
+  end
 
   def self.all
     result = Rails.configuration.api.get('/prisons')
