53758f7ec5bd7fdee37d549c1267aee1c605fbe2
Paul Battley <pbattley@gmail.com>
2016-01-25 10:16:10 +0000

    Obtain prison finder URL from API

--- app/services/link_directory.rb
+++ app/services/link_directory.rb
@@ -4,14 +4,6 @@ class LinkDirectory
   GOOGLE_MAPS = 'http://google.com/maps?q={query}'
   RATE_SERVICE = 'http://www.gov.uk/done/prison-visits'
 
-  def initialize(prison_finder:)
-    @prison_finder_template = URITemplate.new(prison_finder)
-  end
-
-  def prison_finder(prison = nil)
-    @prison_finder_template.expand(prison: prison ? prison.finder_slug : nil)
-  end
-
   def google_maps(query)
     URITemplate.new(GOOGLE_MAPS).expand(query: query)
   end
