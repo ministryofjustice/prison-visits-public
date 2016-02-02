f7d21682bd188e10d2524279e7d758d6c916669e
Martyn Loughran <me@mloughran.com>
2016-02-02 12:43:05 +0000

    Validate visitors
    Adult age is hard-coded for now since until we add an API for visitor constraints

--- app/models/prison.rb
+++ app/models/prison.rb
@@ -2,6 +2,8 @@ class Prison
   include NonPersistedModel
 
   MAX_VISITORS = 6
+  MAX_ADULTS = 3
+  MIN_ADULTS = 1
 
   attribute :id, String
   attribute :name, String
@@ -10,6 +12,7 @@ class Prison
   attribute :email_address, String
   attribute :phone_no, String
   attribute :prison_finder_url, String
+  attribute :adult_age, Integer, default: 18 # Varies for some prisons
 
   def self.find_by_id(id)
     result = Rails.configuration.api.get("/prisons/#{id}")
@@ -19,5 +22,20 @@ class Prison
   def self.all
     result = Rails.configuration.api.get('/prisons')
     result['prisons'].map { |params| Prison.new(params) }
+  end
+
+  def validate_visitor_ages_on(target, field, ages)
+    adults, _children = ages.partition { |a| adult?(a) }.map(&:length)
+    if adults > MAX_ADULTS
+      target.errors.add field, :too_many_adults, max: MAX_ADULTS, age: adult_age
+    elsif adults < MIN_ADULTS
+      target.errors.add field, :too_few_adults, min: MIN_ADULTS, age: adult_age
+    end
+  end
+
+private
+
+  def adult?(age)
+    age >= adult_age
   end
 end
