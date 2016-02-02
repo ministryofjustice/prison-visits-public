11a63b217d6be7c8e9d91f383928c0e52be3a0ae
Martyn Loughran <me@mloughran.com>
2016-02-02 13:01:33 +0000

    Extract booking constraints out of Prison
    Since booking constraints will now be supplied by the API, and since
    in future constraints may depend not only on prison, but on prisoner,
    visitors, etc., it makes more sense to model them separately.
    
    Visitor and Slot constraints are modeled separately, but they’re both loaded
    using the same class (the data for both may come via the same API – TBD).
    
    This commit also fixes all related specs (which were broken since the split).

--- app/models/prison.rb
+++ app/models/prison.rb
@@ -1,10 +1,6 @@
 class Prison
   include NonPersistedModel
 
-  MAX_VISITORS = 6
-  MAX_ADULTS = 3
-  MIN_ADULTS = 1
-
   attribute :id, String
   attribute :name, String
   attribute :address, String
@@ -12,7 +8,6 @@ class Prison
   attribute :email_address, String
   attribute :phone_no, String
   attribute :prison_finder_url, String
-  attribute :adult_age, Integer, default: 18 # Varies for some prisons
 
   def self.find_by_id(id)
     result = Rails.configuration.api.get("/prisons/#{id}")
@@ -22,35 +17,5 @@ class Prison
   def self.all
     result = Rails.configuration.api.get('/prisons')
     result['prisons'].map { |params| Prison.new(params) }
-  end
-
-  def validate_visitor_ages_on(target, field, ages)
-    adults, _children = ages.partition { |a| adult?(a) }.map(&:length)
-    if adults > MAX_ADULTS
-      target.errors.add field, :too_many_adults, max: MAX_ADULTS, age: adult_age
-    elsif adults < MIN_ADULTS
-      target.errors.add field, :too_few_adults, min: MIN_ADULTS, age: adult_age
-    end
-  end
-
-  def last_bookable_date(_today = Time.zone.today)
-    available_slots.sort.last.to_date
-  end
-
-  def bookable_date?(requested_date = Time.zone.today)
-    available_slots.any? { |s| s.to_date == requested_date }
-  end
-
-  def available_slots(_today = Time.zone.today)
-    @available_slots ||= begin
-      api_response = Rails.configuration.api.get('/slots', prison_id: id)
-      api_response['slots'].map { |s| ConcreteSlot.parse(s) }
-    end
-  end
-
-private
-
-  def adult?(age)
-    age >= adult_age
   end
 end
--- spec/helpers/calendar_helper_spec.rb
+++ spec/helpers/calendar_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CalendarHelper do
     it 'enumerates whole weeks from tomorrow to cover month of booking window' do
       #
       #    Mo  Tu  We  Th  Fr  Sa  Su
-      #    23  24  25  26  27a 28  29    First week
+      #    23  24  25  26a 27  28  29    First week
       #    30   1   2   3   4   5   6
       #     7   8   9  10  11  12  13
       #    14  15  16  17  18  19  20
@@ -15,9 +15,11 @@ RSpec.describe CalendarHelper do
       #    a = today; b = end of booking window;
       #    c = end of month concluding window; d = end of week
       #
-      prison = build(:prison, lead_days: 3, booking_window: 28)
+      slots = BookingConstraints::SlotConstraints.new([
+        ConcreteSlot.new(2015, 12, 26, 9, 0, 10, 0)
+      ])
       travel_to Date.new(2015, 11, 26) do # Thursday
-        weeks = helper.weeks(prison)
+        weeks = helper.weeks(slots)
         expect(weeks.length).to eq(6)
         expect(weeks.first.first).to eq(Date.new(2015, 11, 23))
         expect(weeks.last.last).to eq(Date.new(2016, 1, 3))
