0230bbe5884c67303208db39be2aa18df1c08d5a
Martyn Loughran <me@mloughran.com>
2016-02-02 17:46:43 +0000

    Remove now redundant smtp and email healthchecks

--- app/services/healthcheck.rb
+++ app/services/healthcheck.rb
@@ -4,9 +4,7 @@ class Healthcheck
   def initialize
     @components = {
       database: DatabaseCheck.new('Postgres database'),
-      mailers: QueueCheck.new('Email queue', queue_name: 'mailers'),
-      zendesk: QueueCheck.new('Zendesk queue', queue_name: 'zendesk'),
-      smtp: SmtpCheck.new('SMTP server', smtp_settings: smtp_settings)
+      zendesk: QueueCheck.new('Zendesk queue', queue_name: 'zendesk')
     }
   end
 
@@ -18,9 +16,5 @@ class Healthcheck
     @components.inject(ok: ok?) { |hash, (key, checker)|
       hash.merge(key => checker.report)
     }
-  end
-
-  def smtp_settings
-    Rails.configuration.action_mailer.smtp_settings
   end
 end
