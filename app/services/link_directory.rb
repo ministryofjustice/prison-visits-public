d2b507d03777a9e494d4d5493876655cc79e577d
Martyn Loughran <me@mloughran.com>
2016-02-04 16:30:58 +0000

    Reinstate (non-dynamic) prison finder link

--- app/services/link_directory.rb
+++ app/services/link_directory.rb
@@ -3,6 +3,7 @@ require 'uri_template'
 class LinkDirectory
   GOOGLE_MAPS = 'http://google.com/maps?q={query}'
   RATE_SERVICE = 'http://www.gov.uk/done/prison-visits'
+  PRISON_FINDER = 'http://www.justice.gov.uk/contacts/prison-finder'
 
   def google_maps(query)
     URITemplate.new(GOOGLE_MAPS).expand(query: query)
@@ -10,5 +11,9 @@ class LinkDirectory
 
   def rate_service
     RATE_SERVICE
+  end
+
+  def prison_finder
+    PRISON_FINDER
   end
 end
