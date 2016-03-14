2a5fe75afa784517494932cd7b9193d65baa919c
Martyn Loughran <me@mloughran.com>
2016-03-14 10:33:46 +0000

    Re-implement visit cancellation specs
    Also reinstates confirmation check
    
    Note r.e. testing: I did not attempt to port the existing tests across since they are testing the behaviour of visit cancellation which should be unit tested in PVB2.

--- app/controllers/cancellations_controller.rb
+++ app/controllers/cancellations_controller.rb
@@ -1,7 +1,10 @@
 class CancellationsController < ApplicationController
   def create
-    visit = Rails.configuration.pvb_api.cancel_visit(params[:id])
-    redirect_to visit_path(visit.id)
+    visit_id = params[:id]
+    if cancellation_confirmed?
+      Rails.configuration.pvb_api.cancel_visit(visit_id)
+    end
+    redirect_to visit_path(visit_id)
   end
 
 private
