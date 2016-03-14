fce492b1309f2cf6a2a6fcaeae2cd71db615302f
Martyn Loughran <me@mloughran.com>
2016-03-14 10:35:32 +0000

    Add simple spec for visit status page

--- spec/controllers/visits_controller_spec.rb
+++ spec/controllers/visits_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe VisitsController, type: :controller do
-  before do skip 'Status not yet implemented' end
-
   describe 'show' do
-    let(:visit) { create(:visit) }
+    let(:visit) { instance_double(Visit, id: '123456789') }
+    let(:pvb_api) { Rails.configuration.pvb_api }
 
-    it 'assigns the visit to @visit' do
+    it 'calls the get visit API' do
+      expect(pvb_api).to receive(:get_visit).with(visit.id).and_return(visit)
       get :show, id: visit.id, locale: 'en'
       expect(assigns(:visit)).to eq(visit)
     end
