ministryofjustice/prison-visits-2

Show validation error if dates are invalid
Previously, an error would be raised inside Virtus’ date coercion code.

Ideally we wouldn’t be using Virtus since it has significantly different behaviour (coersion and otherwise) to ActiveRecord – which adds significant extra complexity to the app.

spec/models/prisoner_step_spec.rb
@@ -1,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe PrisonerStep do
+  subject { described_class.new(prison: prison) }
+
+  let(:params) {
+    {
+      first_name: 'Joe',
+      last_name: 'Bloggs',
+      date_of_birth: {
+        day: '31',
+        month: '12',
+        year: '1970'
+      },
+      number: 'a1234bc',
+      prison_id: 'uuid'
+    }
+  }
+
+  it 'does not fail if the date is invalid (anti-regression)' do
+    params[:date_of_birth][:month] = '13'
+    prisoner_step = described_class.new(params)
+
+    dob = prisoner_step.date_of_birth
+
+    expect(dob).to be_kind_of(UncoercedDate)
+    expect(dob.month).to eql(13)
+  end
+end

ministryofjustice/prison-visits-public

Conflicts:
	spec/models/prisoner_step_spec.rb

spec/models/prisoner_step_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 RSpec.describe PrisonerStep do
   subject { described_class.new(params) }
 
-  let(:params) { { prison_id: '123' } }
+  let(:params) {
+    {
+      first_name: 'Joe',
+      last_name: 'Bloggs',
+      date_of_birth: {
+        day: '31',
+        month: '12',
+        year: '1970'
+      },
+      number: 'a1234bc',
+      prison_id: '123'
+    }
+  }
   let(:prison) { Prison.new(name: 'Reading Gaol') }
   let(:pvb_api) { PrisonVisits::Api.instance }
 
@@ -14,5 +26,14 @@ RSpec.describe PrisonerStep do
   it 'uses the API in order to determine prison name' do
     expect(pvb_api).to receive(:get_prison).and_return(prison)
     expect(subject.prison_name).to eq('Reading Gaol')
+  end
+
+  it 'does not fail if the date is invalid (anti-regression)' do
+    params[:date_of_birth][:month] = '13'
+
+    dob = subject.date_of_birth
+
+    expect(dob).to be_kind_of(UncoercedDate)
+    expect(dob.month).to eql(13)
   end
 end