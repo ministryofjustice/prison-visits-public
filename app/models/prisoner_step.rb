5b0126250d4fe798206f23105cd80da53e3d8733
Martyn Loughran <me@mloughran.com>
2016-03-29 18:35:15 +0100

    Validate prisoner existence from prisoner step

--- app/models/prisoner_step.rb
+++ app/models/prisoner_step.rb
@@ -15,10 +15,33 @@ class PrisonerStep
   }
   validates :prison_id, presence: true
 
+  validate :validate_prisoner
+
   delegate :name, to: :prison, prefix: true
+
+  I18N_SCOPE = %i[ activemodel errors models prisoner_step api ]
 
   def prison
     # Memoize to avoid multiple API lookups
     @_prison ||= prison_id ? Prison.find_by_id(prison_id) : nil
   end
+
+private
+
+  # rubocop:disable Metrics/AbcSize
+  def validate_prisoner
+    result = PrisonVisits::Api.instance.validate_prisoner(
+      number: number,
+      date_of_birth: date_of_birth
+    )
+
+    return if result.fetch('valid')
+
+    error_nomatch = 'prisoner_does_not_exist'
+    if result.fetch('errors').include?(error_nomatch)
+      errors.add :number, I18n.t(error_nomatch, scope: I18N_SCOPE)
+      errors.add :date_of_birth, I18n.t(error_nomatch, scope: I18N_SCOPE)
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
 end
