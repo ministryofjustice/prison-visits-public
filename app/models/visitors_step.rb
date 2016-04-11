35ecf311ccb967847c5d4225e9240b53b0a519cb
Martyn Loughran <me@mloughran.com>
2016-04-11 15:02:07 +0100

    Show dynamic adult age during booking process

--- app/models/visitors_step.rb
+++ app/models/visitors_step.rb
@@ -21,7 +21,7 @@ class VisitorsStep
   attribute :delivery_error_occurred, Boolean
   attribute :visitors, Array[Visitor]
 
-  delegate :max_visitors, to: :visitor_constraints
+  delegate :max_visitors, :adult_age, to: :visitor_constraints
 
   validates :email_address, presence: true
   validates :phone_no, presence: true, length: { minimum: 9 }
