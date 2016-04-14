9dd4b4170956eaa7033b82dad288c41e138de45b
Martyn Loughran <me@mloughran.com>
2016-04-14 18:12:05 +0100

    Remove redundant database setup from run.sh
    There is no database

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 export RAILS_ENV=production
-cd /usr/src/app
-case ${DOCKER_STATE} in
-create)
-    echo "running create"
-    bundle exec rake db:setup db:seed
-    ;;
-migrate_and_seed)
-    echo "running migrate and seed"
-    bundle exec rake db:migrate db:seed
-    ;;
-esac
 bin/rails server -d --binding 0.0.0.0
 tail -f /usr/src/app/log/logstash_production.json
