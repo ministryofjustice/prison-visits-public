e4be8e1299ba0f3dd6483a64f56ed67f1b7ea93c
Martyn Loughran <me@mloughran.com>
2016-04-20 15:34:02 +0100

    Ar gael yn Gymraeg!

--- spec/features/language_switching_spec.rb
+++ spec/features/language_switching_spec.rb
@@ -6,17 +6,11 @@ RSpec.feature 'Switching languages' do
   include FeaturesHelper
 
   scenario 'switching between available languages' do
-    allow_any_instance_of(ApplicationHelper).
-      to receive(:alternative_locales).and_return([:cy])
-
     visit booking_requests_path(locale: 'en')
 
     expect(page).to have_selector(
       '#proposition-name', text: 'Visit someone in prison'
     )
-
-    allow_any_instance_of(ApplicationHelper).
-      to receive(:alternative_locales).and_return([:en])
 
     click_on('Cymraeg')
 
