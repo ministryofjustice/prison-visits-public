fb93be0891732a577f898b613cd0857fd9fb0ac6
Alan Kennedy <alankennedy100@gmail.com>
2017-03-07 12:11:10 +0000

    Fix feature specs (#121)
    Most likely broken because of a missed rebase on a merged PR.

--- spec/features/maintaining_a_visit_spec.rb
+++ spec/features/maintaining_a_visit_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature 'Maintaining a visit', js: true do
   include FeaturesHelper
 
   scenario 'viewing and withdrawing a visit request', vcr: { cassette_name: :maintianing_a_visit_viewing_and_withdrawing } do
-    visit visit_path(id: '061c604a-6e3e-4b29-97e4-331e8b0194aa', locale: 'en')
+    visit visit_path(id: 'FOOBAR', locale: 'en')
     expect(page).to have_text('Your visit is not booked yet')
 
     click_link 'cancel this visit'
@@ -21,7 +21,7 @@ RSpec.feature 'Maintaining a visit', js: true do
   end
 
   scenario 'viewing and cancelling a booked visit', vcr: { cassette_name: :maintianing_a_visit_viewing_and_cancelling } do
-    visit visit_path(id: '0cd7d165-0463-4024-aacf-849475a4c9fc', locale: 'en')
+    visit visit_path(id: 'FOOBAR', locale: 'en')
     expect(page).to have_text('Your visit has been confirmed')
 
     check_yes_i_want_to_cancel
@@ -30,7 +30,7 @@ RSpec.feature 'Maintaining a visit', js: true do
   end
 
   scenario 'viewing a rejected visit', vcr: { cassette_name: :maintianing_a_visit_viewing_rejected } do
-    visit visit_path(id: '147fe485-ea25-437b-9486-05ad6dfa73e4', locale: 'en')
+    visit visit_path(id: 'FOOBAR', locale: 'en')
     expect(page).to have_text('Your visit request cannot take place')
 
     click_link 'new visit'
@@ -38,7 +38,7 @@ RSpec.feature 'Maintaining a visit', js: true do
   end
 
   scenario 'viewing a withdrawn visit and trying again', vcr: { cassette_name: :maintianing_a_visit_viewing_withdrawn } do
-    visit visit_path(id: 'f74bc736-cdcf-4e03-92e3-df4897b569ac', locale: 'en')
+    visit visit_path(id: 'FOOBAR', locale: 'en')
     expect(page).to have_text('You cancelled this visit request')
 
     click_link 'new visit'
@@ -46,7 +46,7 @@ RSpec.feature 'Maintaining a visit', js: true do
   end
 
   scenario 'viewing a cancelled visit and trying again', vcr: { cassette_name: :maintianing_a_visit_viewing_cancelled } do
-    visit visit_path(id: '93082f13-9194-4a9a-8f0f-cd2aa6add176', locale: 'en')
+    visit visit_path(id: 'FOOBAR', locale: 'en')
     expect(page).to have_text('Your visit is cancelled')
 
     click_link 'new visit'
