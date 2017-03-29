c86339c7cd2fa9b254b5c0b3cd4f58cf71b3d857
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-03-29 12:41:42 +0100

    Add GA dimension metric on number of slot requests

--- app/assets/javascripts/modules/moj.analytics.js
+++ app/assets/javascripts/modules/moj.analytics.js
@@ -1,9 +1,12 @@
-(function () {
+(function() {
   'use strict';
 
   moj.Modules.Analytics = {
-    send: function(gaParams){
+    send: function(gaParams) {
       ga('send', 'event', gaParams.category, gaParams.action, gaParams.label);
+    },
+    setDimension: function(dimensionValue) {
+      ga('set', dimensionValue[0].name, String(dimensionValue[0].value));
     }
   };
 }());
