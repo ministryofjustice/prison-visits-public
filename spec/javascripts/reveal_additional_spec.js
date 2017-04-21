e3969dbcc0a246664037263cdbb9098ecc8b1c73
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-04-21 16:20:29 +0100

    Add/update tests

--- spec/javascripts/reveal_additional_spec.js
+++ spec/javascripts/reveal_additional_spec.js
@@ -1,7 +1,9 @@
+var select;
 describe('Modules.RevealAdditional', function() {
 
   beforeEach(function() {
     loadFixtures('visitors.html');
+    select = $('.js-RevealAdditionalSelect');
   });
 
   describe('...Methods', function() {
@@ -25,13 +27,43 @@ describe('Modules.RevealAdditional', function() {
     });
 
     describe('...after init', function() {
+
       beforeEach(function() {
         moj.Modules.RevealAdditional.init();
       });
 
+      it('select should be 0 by default', function() {
+        expect(select.val()).toBe('0');
+      });
 
-      it('should', function() {
-        var select = $('.js-RevealAdditionalSelect');
+    });
+
+    describe('Clicking Add another visitor', function() {
+
+      beforeEach(function() {
+        moj.Modules.RevealAdditional.init();
+        $('.js-RevealAdditionalButton').click();
+      });
+
+      it('should increment the select list value', function() {
+        expect(select.val()).toBe('1');
+      });
+
+      it('should show one Remove visitor button', function() {
+        expect($('.additional-visitor--last').length).toBe(1);
+      });
+
+    });
+
+    describe('Clicking Remove visitor', function() {
+
+      beforeEach(function() {
+        moj.Modules.RevealAdditional.init();
+        $('.js-RevealAdditionalButton').click();
+      });
+
+      it('should decrement the select list value', function() {
+        $('.js-HideAdditionalButton').last().click();
         expect(select.val()).toBe('0');
       });
 
