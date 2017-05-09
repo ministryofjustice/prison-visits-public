0ebb3e8f4db9ffb3b484005976b293dc4ff0af79
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-05-09 11:54:18 +0100

    Add completion page view tracking with no of slots

--- app/assets/javascripts/modules/moj.AsyncGA.js
+++ app/assets/javascripts/modules/moj.AsyncGA.js
@@ -8,6 +8,7 @@
 
       var gaTrackingId = $(this.el).eq(0).data('ga-tracking-id');
       var hitTypePage = $(this.el).eq(0).data('hit-type-page');
+      var pageView = $(this.el).eq(0).data('page-view');
 
       window.ga = window.ga || function() {
         (ga.q = ga.q || []).push(arguments)
@@ -17,6 +18,8 @@
 
       if (hitTypePage) {
         window.ga('send', 'pageview', location.pathname + '#' + hitTypePage);
+      } else if (pageView) {
+        window.ga('send', 'pageview', pageView);
       } else {
         window.ga('send', 'pageview');
       }
