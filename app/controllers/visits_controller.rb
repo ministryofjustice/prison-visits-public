0d257958b11bb5f2106f357d5bf68a1e4b2bfc1b
Chris Hanes <chris@chrishanes.co.uk>
2017-05-09 16:37:59 +0100

    Add GA request completion tracker with slot count (#155)
    * Add completion page view tracking with no of slots
    
    * Remove custom dimension events
    
    * Change use of referrer for Rails

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,6 +1,7 @@
 class VisitsController < ApplicationController
   def show
     @visit = PrisonVisits::Api.instance.get_visit(params[:id])
+    @request_completed = !request.referer.nil?
     render @visit.processing_state.to_s
   rescue PrisonVisits::APINotFound
     render 'errors/404', status: :not_found
