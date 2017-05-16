4ec6271501f4d435c6c686642f0501ca33472077
Andy White <andy.white@digital.justice.gov.uk>
2017-05-16 11:04:41 +0100

    Use URI for parsing

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -8,5 +8,5 @@ if sentry_dsn
   end
 else
   # (Rails logger is not initialized yet)
-  STDOUT.puts '[WARN] Sentry is not configured (SENTRY_DSN)'
+  STDOUT.puts '[WARN] Sentry DSN is not set (SENTRY_DSN)'
 end
