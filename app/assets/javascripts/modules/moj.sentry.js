d6508b2361fa173c323b7d33924f2733b5f902d5
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-05-19 10:30:34 +0100

    No need to bind Raven to window.onerror

--- app/assets/javascripts/modules/moj.sentry.js
+++ app/assets/javascripts/modules/moj.sentry.js
@@ -7,9 +7,6 @@
       this.raven = Raven;
       this.sentry_js_dsn = $(this.el).data('sentry-js-dsn');
       this.raven.config(this.sentry_js_dsn).install();
-
-      // Capture any uncaught errors
-      // window.onerror = $.proxy(this.handleException, this);
     },
     handleException: function(error) {
       this.raven.captureException(error);
