bf543d8512f3c5925eb1bd023d3c13652b3fda47
Andy White <andy.white@digital.justice.gov.uk>
2017-05-23 16:08:42 +0100

    Revert mistaken change

--- app/assets/javascripts/modules/moj.sentry.js
+++ app/assets/javascripts/modules/moj.sentry.js
@@ -5,7 +5,11 @@
     el: '.js-Sentry',
     init: function() {
       this.raven = Raven;
-      this.raven.config('https://sentry.service.dsd.io/59').install()
+      this.sentry_js_dsn = $(this.el).data('sentry-js-dsn');
+      this.raven.config(this.sentry_js_dsn).install();
+    },
+    handleException: function(error) {
+      this.raven.captureException(error);
     }
   }
 }());
