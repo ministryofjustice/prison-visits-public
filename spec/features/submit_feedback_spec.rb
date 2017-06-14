50afa59b49e37e17989259dc873fa7729a047dd2
Andy White <andy.white@digital.justice.gov.uk>
2017-06-14 16:00:12 +0100

    Fix missing translation on Prisoner DOB

--- spec/features/submit_feedback_spec.rb
+++ spec/features/submit_feedback_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.feature 'Submit feedback', js: true do
   include FeaturesHelper
 
+  # The body contains two fields which can vary:
+  #
+  # * referrer: The port changes from run to run
+  # * user_agent: This differs between local machines and Circle CI
+  #
+  # This matcher truncates these fields.
   normalised_body = lambda do |r1, r2|
     normalised = [r1.body, r2.body].map { |req|
       req.sub(/,"referrer":.+$/, '}')
