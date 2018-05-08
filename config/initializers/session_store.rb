3b27e2b39e9ee4b1cd61e92e2d34461f635e4811
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-05-08 16:07:55 +0100

    Update session cookie
    As part of this Trello card (https://trello.com/c/A2gUGB0u/1369-cookies-tidyup)
    we are changing the name of the session cookie and setting it to expire
    after 30mins.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store,
-  key: '_pvb2_session',
-  secure: Rails.env.production?,
-  httponly: true
+#
+Rails.application.config.session_store :cookie_store, expire_after: 30.minutes,
+                                                      key: '_pvb_public_session',
+                                                      secure: Rails.env.production?,
+                                                      httponly: true
