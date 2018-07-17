a68ea7e6dae3a35f49925204b6bf51a5f7444ded
Chris Hanes <chris@chrishanes.co.uk>
2018-07-17 13:54:53 +0100

    Remove inline GA tracking code
    We can use the GOVUK.analytics module instead

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,21 +2,21 @@ Rails.application.config.content_security_policy do |config|
   config.default_src :self
   config.font_src :self, :data
   config.img_src :self,
-                 :data,
-                 'www.google-analytics.com',
-                 'stats.g.doubleclick.net',
-                 'www.google.co.uk'
+    :data,
+    'www.google-analytics.com',
+    'stats.g.doubleclick.net',
+    'www.google.co.uk'
   config.style_src   :self
   config.connect_src :self
   config.script_src  :self,
-                     'www.google-analytics.com',
-                     'stats.g.doubleclick.net',
-                     "'unsafe-eval'",
-                     "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",  # govuk
-                     "'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g='",  # govuk
-                     "'sha256-9GTWoKmlaDM3V+GStWlXFaD4tf+wPfBN2ds2eySQ9aE='",  # govuk
-                     "'sha256-ilofvlKM19VofnYx59p0jVBmaDFKHc8KYj8rW/jltn0='",  # ga
-                     (Rails.env.test? ? "'unsafe-inline'" : '')
+    'www.google-analytics.com',
+    'stats.g.doubleclick.net',
+    "'unsafe-eval'",
+    "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",  # govuk
+    "'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g='",  # govuk
+    "'sha256-9GTWoKmlaDM3V+GStWlXFaD4tf+wPfBN2ds2eySQ9aE='",  # govuk
+    "'sha256-ilofvlKM19VofnYx59p0jVBmaDFKHc8KYj8rW/jltn0='",  # ga
+    (Rails.env.test? ? "'unsafe-inline'" : '')
 
   # So we can send JS errors to Sentry
   sentry_js_dsn = Rails.configuration.sentry_js_dsn
