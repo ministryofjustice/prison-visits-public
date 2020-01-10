a567466105f39c5960cccdb20762c7b811801f4d
Kath Pobee-Norris <kpobeenorris@gmail.com>
2020-01-10 13:11:04 +0000

    Update LineLength ref to use correct namespace & generally appease the linter:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ locations.each do |location|
   end
 end
 
-Dir[File.expand_path('support/matchers/*.rb', __dir__)].each do |path|
+Dir[File.expand_path('support/matchers/*.rb', __dir__)].sort.each do |path|
   require path
 end
 
