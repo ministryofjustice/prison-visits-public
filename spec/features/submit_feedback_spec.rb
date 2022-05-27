f9d7c3ac13b58aa55d4db19e70c0247ca7617d67
georgeschena <georgeschena@hotmail.com>
2022-05-27 12:55:21 +0100

    Run linter

--- spec/features/submit_feedback_spec.rb
+++ spec/features/submit_feedback_spec.rb
@@ -8,14 +8,4 @@ RSpec.feature 'Submit feedback', js: true do
   # * referrer: The port changes from run to run
   # * user_agent: This differs between local machines and Circle CI
   #
-  # This matcher truncates these fields.
-  normalised_body = lambda do |r1, r2|
-    normalised = [r1.body, r2.body].map { |req|
-      req.sub(/,"referrer":.+$/, '}')
-    }
-
-    normalised.first == normalised.last
-  end
-
-  custom_matchers = [:method, :uri, :host, :path, :valid_uuid, normalised_body]
 end
