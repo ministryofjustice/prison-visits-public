b8943a3fdca870c1ac46d49e03b4383f433386f6
georgeschena <georgeschena@hotmail.com>
2022-05-27 12:39:24 +0100

    Update PRISON_FINDER url

--- app/services/link_directory.rb
+++ app/services/link_directory.rb
@@ -3,7 +3,7 @@ require 'uri_template'
 class LinkDirectory
   GOOGLE_MAPS = 'http://google.com/maps?q={query}'
   RATE_SERVICE = 'http://www.gov.uk/done/prison-visits'
-  PRISON_FINDER = 'http://www.justice.gov.uk/contacts/prison-finder'
+  PRISON_FINDER = 'http://www.gov.uk/government/collections/prisons-in-england-and-wales'
 
   def google_maps(query)
     URITemplate.new(GOOGLE_MAPS).expand(query: query)
