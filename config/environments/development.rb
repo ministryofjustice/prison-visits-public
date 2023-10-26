6d350bff54bd25372caa99ef333b558f7b4d32ee
Serafeim Maroulis <serafeim@hey.com>
2023-10-26 17:28:21 +0100

    Remove duplicate perform_caching = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_controller.perform_caching = false
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
