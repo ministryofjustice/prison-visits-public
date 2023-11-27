f767be7878e8d635a7d435acd428e81cf0f8b14a
Serafeim Maroulis <serafeim@hey.com>
2023-11-27 10:53:46 +0000

    Set action_view.preload_links_header = false
    We use ie stylesheets which results in having them preloaded

--- config/application.rb
+++ config/application.rb
@@ -76,5 +76,8 @@ module PrisonVisits
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # We still use ie stylesheets as well as the govuk_template
+    config.action_view.preload_links_header = false
   end
 end
