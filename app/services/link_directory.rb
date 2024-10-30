b6c765177d0ace20184d241d949aa03b9abd23ec
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-10-30 12:56:59 +0000

    Change feedback link (#1371)
    * Change feedback link
    
    Updates the feedback link
    
    * Update link_directory.rb

--- app/services/link_directory.rb
+++ app/services/link_directory.rb
@@ -1,9 +1,9 @@
 require 'uri_template'
 
 class LinkDirectory
-  GOOGLE_MAPS = 'http://google.com/maps?q={query}'
-  RATE_SERVICE = 'http://www.gov.uk/done/prison-visits'
-  PRISON_FINDER = 'http://www.gov.uk/government/collections/prisons-in-england-and-wales'
+  GOOGLE_MAPS = 'https://google.com/maps?q={query}'
+  RATE_SERVICE = 'https://visit-someone-in-prison.form.service.justice.gov.uk'
+  PRISON_FINDER = 'https://www.gov.uk/government/collections/prisons-in-england-and-wales'
 
   def google_maps(query)
     URITemplate.new(GOOGLE_MAPS).expand(query:)
