6e94773a0d9bdae0bae6c721595c9d77f075cded
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-04-11 13:51:17 +0100

    Fix pre-existing typo

--- app/services/vsip_visit_sessions.rb
+++ app/services/vsip_visit_sessions.rb
@@ -1,7 +1,7 @@
 class VsipVisitSessions
-  def self.get_sessions(nomid_id, prisoner_number)
+  def self.get_sessions(nomis_id, prisoner_number, advanceDays)
     @vsip_enabled_prisons = if Vsip::Api.enabled?
-                              Vsip::Api.instance.visit_sessions(nomid_id, prisoner_number, advanceDays)
+                              Vsip::Api.instance.visit_sessions(nomis_id, prisoner_number, advanceDays)
                             else
                               Vsip::NullSupportedPrisons.new(api_call_successful: false)
                             end
