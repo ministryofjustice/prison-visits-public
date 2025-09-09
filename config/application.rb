4118c083ce2a14da5ac643ba1fbe87a07e38d2dc
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-09-09 13:37:28 +0100

    Rails upgrade, bundle update, load defaults

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 
 module PrisonVisits
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.phase = 'live'
     config.product_type = 'service'
